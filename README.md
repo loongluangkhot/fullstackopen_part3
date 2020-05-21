4b1dbd0fe20028c161c0794be0689624a78b25da
LUANGKHOT Nalongsak (Loong) <loongluangkhot@gmail.com>
2020-05-22 00:34:25 +0800

    uibuild

---
+++ README.md
@@ -0,0 +1,1 @@
+https://phonebook-loongluangkhot.herokuapp.com/
---
+++ build/precache-manifest.630081924666c85372253468fa7a4209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae621a6b9048836d080c88f8f7f7bf03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1beb3e67baaa8d46a1f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "05945b357eb1f5da6df3",
+    "url": "/static/js/2.40776c38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.40776c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1beb3e67baaa8d46a1f",
+    "url": "/static/js/main.40242d5f.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
